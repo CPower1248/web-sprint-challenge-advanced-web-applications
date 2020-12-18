66c397cc2a4673a90c5c7c3eb717e500b0fc88a8
corey-power <CPower1248@gmail.com>
2020-12-18 13:15:33 -0500

    error

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,56 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, useHistory } from "react-router-dom";
+import axios from "axios"
+import PrivateRoute from "./utils/PrivateRoute"
 
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage"
 import "./styles.scss";
 
+const initialLoginForm = {
+  credentials: {
+    username: "",
+    password: ""
+  }
+}
+
 function App() {
+  const [ loginForm, setLoginForm ] = useState(initialLoginForm)
+  const { push } = useHistory()
+
+  const handleChange = e => {
+    setLoginForm({
+      credentials: {
+        ...loginForm.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    axios.post("http://localhost:5000/api/login", loginForm.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload)
+        push("/protected")
+        setLoginForm(initialLoginForm)
+      })
+  }
+
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="username" placeholder="username" onChange={handleChange} value={loginForm.username} />
+          <input type="password" name="password" placeholder="password" onChange={handleChange} value={loginForm.password} />
+          <button>submit</button>
+        </form>
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path="/protected" component={BubblePage} />
       </div>
     </Router>
   );
